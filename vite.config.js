a2e1c761eb3c3830c0f7d5922e8b01506d5bde93
Ecumbe <eduardo.cumbev@ug.edu.ec>
2026-05-07 13:43:40 -0500

    Musicfy v1.0: IA integrada, R2 y Supabase conectados

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // Forzamos el uso de la versión compilada para evitar errores de dependencias de Node/Native
+      'jsmediatags': 'jsmediatags/dist/jsmediatags.min.js'
+    }
+  }
 })
